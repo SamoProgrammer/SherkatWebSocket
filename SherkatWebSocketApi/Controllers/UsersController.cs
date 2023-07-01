b53511a84554a53a699def3f2c018ab8db2d68a6
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-01 23:55:39 +0330

    optimize login

--- SherkatWebSocketApi/Controllers/UsersController.cs
+++ SherkatWebSocketApi/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SherkatWebSocketApi.Authentication;
 using SherkatWebSocketApi.Authentication.Entities;
 using SherkatWebSocketApi.Database;
@@ -13,12 +16,14 @@ namespace SherkatWebSocketApi.Controllers;
 public class UsersController : ControllerBase
 {
     private readonly DatabaseContext _context;
-    private readonly IJWTManagerRepository _jwtManagerRepository;
 
-    public UsersController(DatabaseContext context, JWTManagerRepository jwtManagerRepository)
+    private readonly IConfiguration _configuration;
+
+    public UsersController(DatabaseContext context, IConfiguration configuration)
     {
         _context = context;
-        _jwtManagerRepository = jwtManagerRepository;
+        _configuration = configuration;
+
     }
 
     [HttpPost("Register")]
@@ -98,8 +103,27 @@ public class UsersController : ControllerBase
             return NotFound();
         }
         User user = await _context.Users.FirstAsync(x => x.Username == username);
-        return Ok(_jwtManagerRepository.Authenticate(user));
+        return Ok(Authenticate(user));
 
 
+    }
+    public JWTToken Authenticate(User user)
+    {
+        // Else we generate JSON Web Token
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role),
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(30),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return new JWTToken { Token = tokenHandler.WriteToken(token), Expires = token.ValidTo };
     }
 }
