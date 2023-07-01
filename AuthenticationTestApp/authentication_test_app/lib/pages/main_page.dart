7a2ab47ae75c7a2ac686bfec4e0575e50f9fd044
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-01 23:54:27 +0330

    change view for input

--- AuthenticationTestApp/authentication_test_app/lib/pages/main_page.dart
+++ AuthenticationTestApp/authentication_test_app/lib/pages/main_page.dart
@@ -10,26 +10,35 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final ApiService _apiService = ApiService();
+  TextEditingController txtToken = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextButton(
-        child: const Text('test server'),
-        onPressed: () async {
-          bool result = await _apiService.authenticate();
-          String response = '';
-          final snackBar = SnackBar(content: Text(response));
-          if (result) {
-            setState(() {
-              response = "موفق";
-            });
-          } else {
-            setState(() {
-              response = "ناموفق";
-            });
-          }
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        },
+      body: Column(
+        children: [
+          TextField(
+            controller: txtToken,
+          ),
+          const Padding(padding: EdgeInsets.only(top: 20, bottom: 20)),
+          TextButton(
+            child: const Text('test server'),
+            onPressed: () async {
+              bool result = await _apiService.authenticate(txtToken.text);
+              String response = '';
+              final snackBar = SnackBar(content: Text(response));
+              if (result) {
+                setState(() {
+                  response = "موفق";
+                });
+              } else {
+                setState(() {
+                  response = "ناموفق";
+                });
+              }
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ],
       ),
     );
   }
