33cc9f75b9a94653d606abf57876159a231e53f6
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-01 23:55:07 +0330

    delete repo

--- SherkatWebSocketApi/Authentication/IJWTManagerRepository.cs
+++
@@ -1,8 +0,0 @@
-using SherkatWebSocketApi.Authentication.Entities;
-
-namespace SherkatWebSocketApi.Authentication;
-
-public interface IJWTManagerRepository
-{
-    JWTToken Authenticate(User user); 
-}
