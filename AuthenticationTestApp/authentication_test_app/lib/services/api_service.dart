c5ec0b1cc0c75a778a356d5090d0c8a18000c9d8
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-01 15:03:38 +0330

    use unique_identifier package for unique id

--- AuthenticationTestApp/authentication_test_app/lib/services/api_service.dart
+++ AuthenticationTestApp/authentication_test_app/lib/services/api_service.dart
@@ -1,13 +1,18 @@
 import 'package:http/http.dart' as http;
-import 'package:flutter_udid/flutter_udid.dart';
+import 'package:unique_identifier/unique_identifier.dart';
 
 class ApiService {
   Future<bool> authenticate() async {
-    String udid = await FlutterUdid.udid;
-    var url = Uri.https('http://localhost:5034', 'devices/authenticate');
-    var response = await http.post(url, body: {'androidId': udid});
+    String? identifier = await UniqueIdentifier.serial;
+    var url = Uri.http('http://localhost:5034', 'api/Devices/Register');
+    var response = await http
+        .post(url, body: {'deviceId': identifier, 'location': 'mashhad'});
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
-    return true;
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
