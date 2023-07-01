3cedd48f33a2a286283f10b2a43e9759f6334f32
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-01 15:07:21 +0330

    view model for entities

---
+++ SherkatWebSocketApi/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,8 @@
+namespace SherkatWebSocketApi.ViewModels;
+
+public class DeviceViewModel
+{
+    public string DeviceId { get; set; }
+    public string AdminUsername { get; set; }
+    public string Location { get; set; }
+}
---
+++ SherkatWebSocketApi/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+namespace SherkatWebSocketApi.ViewModels;
+
+public class UserViewModel
+{
+    public string Username { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
