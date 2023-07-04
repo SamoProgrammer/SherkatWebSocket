225ce5bfe5af8a2428f6b20a84fee64f7fabf8f5
SamoProgrammer <samo092813463@gmail.com>
2023-07-04 15:37:58 +0330

    bearer token header for register device request

--- AuthenticationTestApp/authentication_test_app/lib/services/api_service.dart
+++ AuthenticationTestApp/authentication_test_app/lib/services/api_service.dart
@@ -2,11 +2,17 @@ import 'package:http/http.dart' as http;
 import 'package:unique_identifier/unique_identifier.dart';
 
 class ApiService {
-  Future<bool> authenticate() async {
+  Future<bool> authenticate(String token) async {
     String? identifier = await UniqueIdentifier.serial;
     var url = Uri.http('http://localhost:5034', 'api/Devices/Register');
-    var response = await http
-        .post(url, body: {'deviceId': identifier, 'location': 'mashhad'});
+    var response = await http.post(url, body: {
+      'deviceId': identifier,
+      'location': 'mashhad'
+    }, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
     if (response.statusCode == 200) {
